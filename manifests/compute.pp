f3af1f6fc1d53018132b283f7a0cccce9fd92b76
Atze de Vries <atze.devries@naturalis.nl>
2014-02-25 11:30:10 +0100

    add comma

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -28,7 +28,7 @@ class role_openstack::compute(
 
   exec {'apt-get-update after repo addition':
     command => '/usr/bin/apt-get update',
-    unless => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-cloud-archive.list'
+    unless => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-cloud-archive.list',
     before => Class['openstack::compute'],
   }
 
--- manifests/control.pp
+++ manifests/control.pp
@@ -27,7 +27,7 @@ class role_openstack::control(
 
   exec {'apt-get-update after repo addition':
     command => '/usr/bin/apt-get update',
-    unless => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-cloud-archive.list'
+    unless => '/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-cloud-archive.list',
     before => Class['openstack::controller'],
   }
 
