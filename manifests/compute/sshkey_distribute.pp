f8a32f8ebf63bbc738a57cfc495f0720bd2aac25
Atze de Vries <atze.devries@naturalis.nl>
2014-03-07 11:28:49 +0100

    ssh config

--- manifests/compute/sshkey_distribute.pp
+++ manifests/compute/sshkey_distribute.pp
@@ -34,6 +34,7 @@ class role_openstack::compute::sshkey_distribute(
     command => "/bin/su -s '/bin/bash' -c '/usr/bin/ssh-keygen -b 2048 -t rsa -f /var/lib/nova/.ssh/id_rsa -q -N \"\"' nova",
     creates => '/var/lib/nova/.ssh/id_rsa.pub',
     require => File["nova-ssh-dir"],
+    notify  => Exec["${::fqdn}-fact-pub-sshkey"],
   }
  
 
@@ -43,16 +44,19 @@ class role_openstack::compute::sshkey_distribute(
     command => '/bin/echo nova_pub_sshkey=$(/bin/cat /var/lib/nova/.ssh/id_rsa.pub) > /etc/facter/facts.d/nova_pub_sshkey.txt',
     creates => '/etc/facter/facts.d/nova_pub_sshkey.txt',
     require => Exec["${::fqdn}-generate-sshkey"],
+    refreshonly => true,
+    notify => Exec["${::fqdn}-add-pub-key"],
   }
 
 
   if ($::nova_pub_sshkey) {
     @@exec {"${::fqdn}-add-pub-key":
-      command => "/bin/echo ${::nova_pub_sshkey} >> /var/lib/nova/.ssh/authorized_keys",
+      command     => "/bin/echo ${::nova_pub_sshkey} >> /var/lib/nova/.ssh/authorized_keys",
       #creates => '/var/lib/nova/.ssh/authorized_keys',
-      unless  => "/bin/cat /var/lib/nova/.ssh/authorized_keys | /bin/grep '${::nova_pub_sshkey}'", 
-      require => File["nova-ssh-dir"],
-      tag     => $export_tag,
+      unless      => "/bin/cat /var/lib/nova/.ssh/authorized_keys | /bin/grep '${::nova_pub_sshkey}'", 
+      require     => File["nova-ssh-dir"],
+      refreshonly => true,
+      tag         => $export_tag,
     }
 
 #    @@exec {"${::fqdn}-add-pub-key-to-known-hosts":
