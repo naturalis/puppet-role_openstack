21778559bd855cbc92bde5959c6875be923ab025
Atze de Vries <atze.devries@naturalis.nl>
2014-06-11 16:03:47 +0200

    added stages

---
+++ manifests/control/prepare.pp
@@ -0,0 +1,64 @@
+class role_openstack::control::prepare(
+  
+  )
+{
+
+  file {'/etc/network/interfaces':
+    ensure    => present,
+    mode      => '0644',
+    content   => template('role_openstack/interfaces.erb'),
+  }
+
+  exec {'set interface eth1 to up':
+    command   => '/sbin/ifconfig eth1 up',
+    unless    => '/sbin/ifconfig | /bin/grep eth1',
+    require   => File['/etc/network/interfaces'],
+  }
+
+  # do use local storage for glance/cinder
+  package {'lvm2':
+    ensure => present,
+  }
+
+  exec {"/sbin/pvcreate ${lvm_volume_disks}":
+    unless   => "/sbin/pvdisplay ${lvm_volume_disks}",
+    require  => Package['lvm2'],
+    stage => 'pre',
+  }
+
+  exec {"/sbin/vgcreate cinder-volumes ${lvm_volume_disks}":
+    unless   => "/sbin/vgdisplay cinder-volumes",
+    require  => Exec["/sbin/pvcreate ${lvm_volume_disks}"],
+    before   => Apt::Source['ubuntu-cloud-archive'],
+  }
+
+
+  package { 'ethtool':
+    ensure => present,
+  }
+
+  # this is to fix network speed
+  exec { 'set gro to off':
+    command => '/sbin/ethtool --offload eth1 gro off',
+    unless  => '/sbin/ethtool --show-offload eth1 | /bin/grep generic-receive-offload | /bin/grep off',
+    require => Package['ethtool'],
+  }
+
+  package {'ubuntu-cloud-keyring':
+    ensure => present,
+    before =>  Apt::Source['ubuntu-cloud-archive'],
+  }
+
+  apt::source { 'ubuntu-cloud-archive':
+    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+    release           => "precise-updates/havana",
+    repos             => 'main',
+  #  required_packages => 'ubuntu-cloud-keyring',
+  } ~>
+
+  exec {'apt-get-update after repo addition':
+    command       => '/usr/bin/apt-get update',
+    refreshonly  => true,
+  }
+
+}
--- manifests/control/stackinstance.pp
+++ manifests/control/stackinstance.pp
@@ -33,72 +33,11 @@ class role_openstack::control::stackinstance(
     before => Stage['main'],
   }
 
+  class {'::role_openstack::control::prepare':
+    stage => 'pre',
+  }
+
  #configure eth1 to be up
-  file {'/etc/network/interfaces':
-    ensure    => present,
-    mode      => '0644',
-    content   => template('role_openstack/interfaces.erb'),
-    stage => 'pre',
-  }
-
-  exec {'set interface eth1 to up':
-    command   => '/sbin/ifconfig eth1 up',
-    unless    => '/sbin/ifconfig | /bin/grep eth1',
-    require   => File['/etc/network/interfaces'],
-    stage   => 'pre',
-  }
-
-  # do use local storage for glance/cinder
-  package {'lvm2':
-    ensure => present,
-    stage => 'pre',
-  }
-
-  exec {"/sbin/pvcreate ${lvm_volume_disks}":
-    unless   => "/sbin/pvdisplay ${lvm_volume_disks}",
-    require  => Package['lvm2'],
-    stage => 'pre',
-  }
-
-  exec {"/sbin/vgcreate cinder-volumes ${lvm_volume_disks}":
-    unless   => "/sbin/vgdisplay cinder-volumes",
-    require  => Exec["/sbin/pvcreate ${lvm_volume_disks}"],
-    before   => Apt::Source['ubuntu-cloud-archive'],
-    stage => 'pre',
-  }
-
-
-  package { 'ethtool':
-    ensure => present,
-    stage => 'pre',
-  }
-
-  # this is to fix network speed
-  exec { 'set gro to off':
-    command => '/sbin/ethtool --offload eth1 gro off',
-    unless  => '/sbin/ethtool --show-offload eth1 | /bin/grep generic-receive-offload | /bin/grep off',
-    require => Package['ethtool'],
-    stage => 'pre',
-  }
-
-  package {'ubuntu-cloud-keyring':
-    ensure => present,
-    before =>  Apt::Source['ubuntu-cloud-archive'],
-    stage => 'pre',
-  }
-
-  apt::source { 'ubuntu-cloud-archive':
-    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
-    release           => "precise-updates/havana",
-    repos             => 'main',
-  #  required_packages => 'ubuntu-cloud-keyring',
-  } ~>
-
-  exec {'apt-get-update after repo addition':
-    command       => '/usr/bin/apt-get update',
-    refreshonly  => true,
-    stage => 'pre',
-  }
 
   #include apt::update
 
